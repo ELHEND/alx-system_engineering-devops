cb7b87c6dd8e842203b045419a30bcefdf024481
ELHEND <alhnd683@gmail.com>
2023-11-07 01:49:30 -0800

    koko

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Contains recurse function"""
+""" the Contains recurse function"""
+
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
+    """ the Returns  list of titles of all hot posts on  given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
